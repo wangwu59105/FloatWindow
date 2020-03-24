972054573be9b0c74009569254bbae7178700e48
wangwu <13477037503@sina.cn>
2020-03-24 16:36:57 +0800

    androidx

--- floatwindow/src/main/java/com/yhao/floatwindow/Util.java
+++ floatwindow/src/main/java/com/yhao/floatwindow/Util.java
@@ -6,7 +6,7 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.annotation.RequiresApi;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
@@ -27,6 +27,38 @@ class Util {
     }
 
     private static Point sPoint;
+
+
+//    /**
+//     * 获得屏幕高度
+//     *
+//     * @param context
+//     * @return
+//     */
+//    public static int getScreenWidth(Context context) {
+//        WindowManager wm = (WindowManager) context
+//                .getSystemService(Context.WINDOW_SERVICE);
+//        DisplayMetrics outMetrics = new DisplayMetrics();
+//        wm.getDefaultDisplay().getMetrics(outMetrics);
+//        return outMetrics.widthPixels;
+//    }
+//
+//    /**
+//     * 获得屏幕宽度
+//     *
+//     * @param context
+//     * @return
+//     */
+//    public static int getScreenHeight(Context context) {
+//        WindowManager wm = (WindowManager) context
+//                .getSystemService(Context.WINDOW_SERVICE);
+//        DisplayMetrics outMetrics = new DisplayMetrics();
+//        wm.getDefaultDisplay().getMetrics(outMetrics);
+//        return outMetrics.heightPixels;
+//    }
+
+
+
 
     static int getScreenWidth(Context context) {
         if (sPoint == null) {
--- sample/src/main/java/com/example/yhao/floatwindow/BaseActivity.java
+++ sample/src/main/java/com/example/yhao/floatwindow/BaseActivity.java
@@ -1,7 +1,8 @@
 package com.example.yhao.floatwindow;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.yhao.fixedfloatwindow.R;
 
